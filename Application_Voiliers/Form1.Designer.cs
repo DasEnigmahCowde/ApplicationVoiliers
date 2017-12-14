6e0d9a55455ce438e6baf50ea5a493d56d9b12e5
GOUPILLOT Alexis <alexis.goupillot@epsi.fr>
2017-12-14 19:16:29 +0100

    Debut preparation du WinForms avec BDD

---
+++ Application_Voiliers/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Application_Voiliers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Ajouter = new System.Windows.Forms.Button();
+            this.chp_NomV = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chp_ImmatV = new System.Windows.Forms.TextBox();
+            this.chp_MarqueV = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Ajouter
+            // 
+            this.btn_Ajouter.Location = new System.Drawing.Point(366, 302);
+            this.btn_Ajouter.Name = "btn_Ajouter";
+            this.btn_Ajouter.Size = new System.Drawing.Size(75, 23);
+            this.btn_Ajouter.TabIndex = 0;
+            this.btn_Ajouter.Text = "Ajouter";
+            this.btn_Ajouter.UseVisualStyleBackColor = true;
+            this.btn_Ajouter.Click += new System.EventHandler(this.btn_Ajouter_Click);
+            // 
+            // chp_NomV
+            // 
+            this.chp_NomV.Location = new System.Drawing.Point(353, 100);
+            this.chp_NomV.Name = "chp_NomV";
+            this.chp_NomV.Size = new System.Drawing.Size(100, 22);
+            this.chp_NomV.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 370);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(801, 234);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // chp_ImmatV
+            // 
+            this.chp_ImmatV.Location = new System.Drawing.Point(353, 247);
+            this.chp_ImmatV.Name = "chp_ImmatV";
+            this.chp_ImmatV.Size = new System.Drawing.Size(100, 22);
+            this.chp_ImmatV.TabIndex = 3;
+            // 
+            // chp_MarqueV
+            // 
+            this.chp_MarqueV.Location = new System.Drawing.Point(353, 175);
+            this.chp_MarqueV.Name = "chp_MarqueV";
+            this.chp_MarqueV.Size = new System.Drawing.Size(100, 22);
+            this.chp_MarqueV.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(350, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nom du Voilier ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(350, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Marque du Voilier ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(335, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Immatriculation du Voilier";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 616);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chp_MarqueV);
+            this.Controls.Add(this.chp_ImmatV);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.chp_NomV);
+            this.Controls.Add(this.btn_Ajouter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Ajouter;
+        private System.Windows.Forms.TextBox chp_NomV;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox chp_ImmatV;
+        private System.Windows.Forms.TextBox chp_MarqueV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Application_Voiliers/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Application_Voiliers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Ajouter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message d'erreur");
+            }
+        }
+    }
+}
